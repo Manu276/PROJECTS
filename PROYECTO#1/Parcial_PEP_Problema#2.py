887d795e605ad72b3dfb40c081e33647f17d03c9
Manu276 <115639878+Manu276@users.noreply.github.com>
2023-07-10 22:45:23 -0400

    Update Parcial_PEP_Problema#2.py

--- PROYECTO#1/Parcial_PEP_Problema#2.py
+++ PROYECTO#1/Parcial_PEP_Problema#2.py
@@ -1,3 +1,6 @@
+Luzmar Almao  C.I: 27296990
+Manuel Planas C.I: 27373001
+
 H = float(input("Ingrese la profundidad del pozo en metros: "))
 Ld = float(input("Ingrese la distancia que el caracol sube durante el día en metros: "))
 Ln = float(input("Ingrese la distancia que el caracol desciende durante la noche en metros: "))
